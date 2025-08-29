7cb3851f5081b3e0b0f9e27bfd405f9988bb45e1
lalit-collab <lalitkumar.insights@gmail.com>
2025-08-29 11:19:37 +0530

    commit

--- array-processor-api/src/routes/arrayRoutes.js
+++ array-processor-api/src/routes/arrayRoutes.js
@@ -16,6 +16,15 @@ router.get('/', (req, res) => {
   });
 });
 
+router.get('/bfhl', (req, res) => {
+  res.status(200).json({
+    message: 'This endpoint requires a POST request with array data',
+    example_request: {
+      data: ["a", "1", "334", "4", "R", "$"]
+    },
+    documentation: "See the root path (/) for API information"
+  });
+});
 // BFHL route - main functionality
 router.post('/bfhl', arrayController.processArray);
 
